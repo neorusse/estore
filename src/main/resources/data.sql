8b743ccde0e13bbe3c3a08e6a4aaa57f01a884b3
neorusse <neorusse@gmail.com>
2020-05-16 12:46:04 +0100

    Add one more insert query for item table

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,2 +1,3 @@
 insert into item (name, price, description) values ('Iphone11', 850.99, 'A phone like no other');
 insert into item (name, price, description) values ('AppleTv', 320.99, 'Redefining Tv experience');
+insert into item (name, price, description) values ('Ipad', 720.99, 'Your replacement laptop machine');
