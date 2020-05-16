24b816dd3eea23986bed23a7a0006da308e1a44d
neorusse <neorusse@gmail.com>
2020-05-16 10:16:17 +0100

    Initialize item table

---
+++ src/main/resources/data.sql
@@ -0,0 +1,2 @@
+insert into item (name, price, description) values ('Iphone 11', 850.99, 'A phone like no other');
+insert into item (name, price, description) values ('Apple Tv', 320.99, 'Redefining Tv experience');
