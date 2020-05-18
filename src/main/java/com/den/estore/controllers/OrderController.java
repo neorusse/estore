cda4c30d18b7ddb355ed5150267e36ce86337519
neorusse <neorusse@gmail.com>
2020-05-18 08:44:01 +0100

    Define logging for order end points

--- src/main/java/com/den/estore/controllers/OrderController.java
+++ src/main/java/com/den/estore/controllers/OrderController.java
@@ -4,6 +4,8 @@ import com.den.estore.io.entity.UserOrder;
 import com.den.estore.io.entity.Users;
 import com.den.estore.io.repository.OrderRepository;
 import com.den.estore.io.repository.UserRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -14,6 +16,9 @@ import java.util.List;
 @RequestMapping("/api/order")
 public class OrderController {
 
+  // adding logs
+  private final Logger logger = LoggerFactory.getLogger(OrderController.class);
+
   @Autowired
   private UserRepository userRepository;
 
@@ -23,6 +28,8 @@ public class OrderController {
 
   @PostMapping("/submit/{username}")
   public ResponseEntity<UserOrder> submit(@PathVariable String username) {
+
+    logger.info("HTTP POST request received at /api/order/submit/{username} url");
 
     Users user = userRepository.findByUsername(username);
 
@@ -39,6 +46,8 @@ public class OrderController {
 
   @GetMapping("/history/{username}")
   public ResponseEntity<List<UserOrder>> getOrdersForUser(@PathVariable String username) {
+
+    logger.info("HTTP GET request received at /api/order/history/{username} url");
 
     Users user = userRepository.findByUsername(username);
 
